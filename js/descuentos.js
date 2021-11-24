5453f929d1d394aba6ff2c4e0849d6cd6da4e8dd
Johan Meneses <johanmeci03@gmail.com>
2021-11-24 14:04:30 -0500

    Agregando taller porcentajes y descuentos

--- js/descuentos.js
+++ js/descuentos.js
@@ -1,3 +1,8 @@
+//Variables
+const btnGetTotal = document.getElementById('btnTotal');
+const spanResult = document.querySelector('#result-descuento');
+
+
 //Funciones
 function calcularPrecioConDescuento(precio, descuento) {
 
@@ -7,3 +12,16 @@ function calcularPrecioConDescuento(precio, descuento) {
     return precioConDescuento;
     
 }
+
+//Listener Click
+btnGetTotal.addEventListener('click', () => {
+
+    const precioOriginal = document.getElementById('inputPrecio').value;
+    const porcentajeDescuento = Number.parseInt(document.getElementById('inputDescuento').value);
+
+    const totalPrecioDescuento = calcularPrecioConDescuento(precioOriginal, porcentajeDescuento);
+
+    spanResult.parentElement.classList.add('active');
+    spanResult.firstElementChild.textContent = `Total: $${totalPrecioDescuento}`;
+
+});
